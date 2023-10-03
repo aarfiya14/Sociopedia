d5cb2cb8ae25419e74fc8ce9a66479960d688a12
Aarfiya Sarfaraz <aarfiyasarfaraz1408@gmail.com>
2023-10-03 21:00:16 +0530

    first commit

---
+++ client/src/scenes/footer/index.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
+const Footer = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  const theme = useTheme(); //automatically detects color mode in the project based on mode
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+  //there we used = instead of : as material ui gives us box components on which we can directly apply css.(Only available for box components)
+  return (
+    <FlexBetween
+      padding="1rem 6%"
+      backgroundColor={alt}
+      style={{ fontWeight: "bold" }}
+    >
+      <div style={{ width: "100%", textAlign: "center" }}>
+        Made by Shashwat Anand @ 2023
+      </div>
+    </FlexBetween>
+  );
+};
+
+export default Footer;
---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,51 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navabr";
+import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import Footer from "scenes/footer";
+const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+  return (
+    <Box>
+      <Box width="100%" position="fixed" zIndex="1">
+        <Navbar />
+      </Box>
+
+      <Box
+        width="100%"
+        padding="6rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} stick />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0"></Box>
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
+      </Box>
+      <Box width="100%">
+        <Footer />
+      </Box>
+    </Box>
+  );
+};
+
+export default HomePage;
